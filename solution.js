d355e30310d72af2edff88f54d450991f73b0284
Saad Malik <saad_malik200@hotmail.com>
2022-06-29 23:31:00 +0200

    finshed ex2

--- solution.js
+++ solution.js
@@ -1,1 +1,19 @@
+const str = "somestring";
+console.log(str);
+console.log(str[1], str[5]);
 
+const firstName = "saad";
+const age = 31;
+const year = 2022;
+
+console.log(firstName + " is " + age + " in " + year);
+
+const isMarried = false;
+
+console.log(firstName + " is married: " + isMarried);
+
+const programmingLanguage = "JavaScript";
+const isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+console.log(programmingLanguage[0], programmingLanguage[4]);
+console.log(programmingLanguage.length);
